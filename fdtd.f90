182275d12e8d362a97fa1aef23e6749baeeb1ae3
J MN <jomarno530.git@gmail.com>
2020-11-09 18:16:52 +0100

    Changed format for .csv output

--- fdtd.f90
+++ fdtd.f90
@@ -1,22 +1,11 @@
-function filename(n) result(name)
-    character(len=20) :: name
-    character(len=20) :: n2str
-    integer :: n
-    write (n2str,"(I3.3)") n
-    name = "frame"//trim(n2str)//".csv"
-end function filename
-
 program fdtd
     implicit none
     integer, parameter :: length=4
     ! real, parameter :: c0=299792458.
-    real, dimension(length) :: epsilon_r, mu_r, &
-    mE, mH, Ey, Hx
+    real, dimension(length) :: epsilon_r, mu_r, mE, mH, Ey, Hx
     real :: dz=1
-    integer :: k, step, steps_per_frame=10, &
-    frame, number_of_frames=4
-    character(len=20) :: filename
-
+    integer :: k, step, steps_per_frame=10, frame, number_of_frames=4
+    character(len=20) :: filename, format_string='(E16.9)'
     ! Initialize materials to free space
     ! Initialize fields to zero
     do k=1, length
@@ -41,14 +30,30 @@ program fdtd
     do frame = 1, number_of_frames
         
         ! write (filename,*) frame
-        print *, ""
-        print *, filename(frame)
-        print *, ""
-        print *, Hx
-        print *, ""
-        print *, Ey
-        print *, ""
+        print *, ''
+        write (filename,'("frame"I4.4".csv")') frame
+        print *, filename
         
+        print *, ''
+
+        do k = 1, length-1
+            write(*,format_string,advance='NO') Hx(k)
+            write(*,'(A)',advance='NO') ','
+        end do
+        write(*,format_string,advance='NO') Hx(1)
+        
+        print *, ''
+        print *, ''
+
+        write(*,format_string,advance='NO') Ey(1)
+        do k = 2, length
+            write(*,'(A)',advance='NO') ','
+            write(*,format_string,advance='NO') Ey(k)
+        end do
+
+        print *, ''
+        print *, ''
+
         do step = 1, steps_per_frame
             
             do k = 1, length-1
@@ -67,3 +72,17 @@ program fdtd
 
 
 end program fdtd
+
+! function filename(n) result(name)
+!     character(len=20) :: name
+!     character(len=20) :: n2str
+!     integer :: n
+!     write (n2str,'(I4.4)') n
+!     name = 'frame'//trim(n2str)//'.csv'
+! end function filename
+
+! function filename(n) result(name)
+!     character(len=20) :: name
+!     integer :: n
+!     write (name,'("frame"I4.4".csv")') n
+! end function filename
