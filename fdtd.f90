89db9d31a3dbda283ef3db9c45671a0f4432e904
J MN <jomarno530.git@gmail.com>
2020-11-24 18:02:05 +0100

    Materials are now read from a separate file

--- fdtd.f90
+++ fdtd.f90
@@ -3,37 +3,43 @@ program fdtd
     integer :: length, number_of_frames, steps_per_frame
     integer :: k, frame, step
     real :: c0, dz, tau, t0
+    real :: dt, h1, h2, h3, e1, e2, e3
     real, allocatable, dimension(:) :: epsilon_r, mu_r, mE, mH, Ey, Hx
     real, allocatable, dimension(:) :: sourceEy, sourceHx
-    real :: dt, h1, h2, h3, e1, e2, e3
     character(len=20) :: filename, format_string
 
+    ! Read parameters from CSV file
     open(1, file='parameters.csv', status='old')
     read(1,*) length, number_of_frames, steps_per_frame, c0, dz, tau, t0
     close(1)
 
-    allocate(epsilon_r(length), mu_r(length))
-    allocate(mE(length), mH(length), Ey(length), Hx(length))
-    allocate(sourceEy(number_of_frames*steps_per_frame))
-    allocate(sourceHx(number_of_frames*steps_per_frame))
+    allocate(epsilon_r(length), mu_r(length), mE(length), mH(length))
+
+    open(1, file='./materials/mu_r.csv', status='old')
+    read(1,*) mu_r
+    close(1)
+    open(2, file='./materials/epsilon_r.csv', status='old')
+    read(2,*) epsilon_r
+    close(2)
+
+    ! Compute update coefficients
+    mE = 0.5*sqrt(epsilon_r(1)*mu_r(1))*dz/epsilon_r
+    mH = 0.5*sqrt(epsilon_r(1)*mu_r(1))*dz/mu_r
+    ! dt = n*dz/(2*c0)
+    ! n := refractive index at boundries
+    ! mE = c0*dt/epsilon_r = n*dz/(2*epsilon_r)
+    ! mH = c0*dt/mu_r = n*dz/(2*mu_r)
 
     ! Compute time step
-    dt = 0.5*dz/c0
+    dt = 0.5*sqrt(epsilon_r(1)*mu_r(1))*dz/c0
 
+    deallocate(epsilon_r, mu_r)
+
+    ! Initialize fields to zero
+    allocate(Ey(length), Hx(length))
     do k=1, length
-        ! Initialize materials to free space
-        epsilon_r(k)=1
-        mu_r(k)=1
-
-        ! Initialize fields to zero
         Hx(k)=0
         Ey(k)=0
-    end do
-
-    ! Add chunk of material
-    do k=2*length/3, 3*length/4
-        epsilon_r(k)=2.4
-        mu_r(k)=2.4
     end do
 
     ! Initialize boundary terms to zero
@@ -41,21 +47,12 @@ program fdtd
     e1=0;e2=0;e3=0
 
     ! Generate source
+    allocate(sourceEy(number_of_frames*steps_per_frame))
+    allocate(sourceHx(number_of_frames*steps_per_frame))
     do step=1, number_of_frames*steps_per_frame
         sourceEy(step)= exp(-((step*dt-t0)/tau)**2)
         sourceHx(step)= -exp(-(((step+1.5)*dt-t0)/tau)**2)
     end do
-
-
-    ! Compute update coefficients
-    mE = 0.5*dz/epsilon_r
-    mH = 0.5*dz/mu_r
-    
-    ! dt = n*dz/(2*c0)
-    ! n := refractive index at boundries
-    ! mE = c0*dt/epsilon_r = n*dz/(2*epsilon_r)
-    ! mH = c0*dt/mu_r = n*dz/(2*mu_r)
-
 
     ! Format for output
     format_string = 'E16.9'
