80195e16069e8221a7d404a4a08f79b1f168409b
J MN <jomarno530.git@gmail.com>
2020-12-17 18:45:27 +0100

    renderframes.py now reads material properties to draw background

--- renderframes.py
+++ renderframes.py
@@ -9,6 +9,7 @@ Created on Mon Nov  9 19:41:08 2020
 import glob
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import colors as mcolors
 
 def read_parameters():
     parameters = list(np.genfromtxt('parameters.csv', delimiter=','))
@@ -16,16 +17,22 @@ def read_parameters():
         parameters[i] = int(parameters[i])
     return parameters
 
+def read_refractive_index():
+    epsilon_r = np.genfromtxt('./materials/epsilon_r.csv', delimiter=',')
+    mu_r = np.genfromtxt('./materials/mu_r.csv', delimiter=',')
+    return np.sqrt(epsilon_r*mu_r)
+
 plt.close('all')
 plt.ioff()
     
 length, number_of_frames, steps_per_frame, ks, \
     c0, dz, tau, t0 = read_parameters()
 
+n = read_refractive_index()
+n_norm = (n-np.min(n))/(np.max(n)-np.min(n))
+
 zH = (np.arange(length) + 0.5)*dz
 zE = np.arange(length)*dz
-
-i = 0
 
 fig, ax = plt.subplots()
 ax.set_ylim(-1.5, 1.5)
@@ -35,16 +42,18 @@ ax.set_xlim(0, length)
 # plt.grid(True, 'minor', alpha=0.2, linewidth=0.6)
 ax.axvline((ks-1)*dz, color='k', linestyle=':')
 
-# ax.axvline(60*dz, color='darkgrey')
-# ax.axvline(80*dz, color='darkgrey')
-
 fig.patch.set_facecolor('darkgrey')
-# ax.patch.set_facecolor('lightgrey')
-ax.axvspan(0, 60, facecolor='lightgrey')
-ax.axvspan(60, 80, facecolor='silver')
-ax.axvspan(80, 120, facecolor='lightgrey')
+
+grey1 = mcolors.to_rgb('lightgrey')[0]
+grey2 = mcolors.to_rgb('silver')[0]
+greyvalues = grey1 + n_norm*(grey2-grey1)
+
+for i in range(len(greyvalues)):
+    color = (greyvalues[i], greyvalues[i], greyvalues[i])
+    ax.axvspan(i*dz, (i+1)*dz, facecolor=color)
 
 files = glob.glob('output/Hx*')
+i = 0
 for filename in files:
     Hx = np.genfromtxt(filename, delimiter=',')
     Ey = np.genfromtxt('output/Ey' + filename[-8:], delimiter=',')
